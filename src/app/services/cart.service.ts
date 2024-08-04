a8fde3c11fd0d3d35c68d0431027e3a79a0ff026
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-08-05 00:06:10 +0300

    add shopping cart item crud

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -40,7 +40,8 @@ export class CartService {
     this.computeCartTotals();
   }
 
-  private computeCartTotals() {
+
+  computeCartTotals() {
     let totalPriceValue: number = 0;
     let totalQuantityValue: number = 0;
 
@@ -65,5 +66,29 @@ export class CartService {
     }
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
     console.log('-------')
+  }
+
+  decrementQuantity(theCartItem: CartItem){
+    theCartItem.quantity--;
+
+    if(theCartItem.quantity === 0){
+      this.remove(theCartItem);
+    }
+    else {
+      this.computeCartTotals();
+    }
+  }
+
+  remove (theCartItem: CartItem) {
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex(
+      tempCartItem => tempCartItem.id === theCartItem.id
+    )
+    // if found, remove the item from the array at the given index
+    if (itemIndex > -1){
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
   }
 }
